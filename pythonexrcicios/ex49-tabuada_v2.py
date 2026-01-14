6934e2c7427edd91c7a05bbc3858090c6a5b1f6f
Hortencio-Arthur <148153337+Hortencio-Arthur@users.noreply.github.com>
2026-01-14 11:28:24 -0300

    ex49-50

---
+++ pythonexrcicios/ex49-tabuada_v2.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero inteiro: '))
+
+print('-=' * 8)
+for c in range(1, 11):
+    print(f'{n} x {c} = {n * c}')
+print('-=' * 8)
---
+++ pythonexrcicios/ex50-soma_pares.py
@@ -0,0 +1,9 @@
+s = 0
+con = 0
+for c in range (1,7):
+    n = int(input(f'{c}° numero: '))f
+    if n % 2 == 0:
+        s += n
+        con +=1
+
+print(f'A soma dos {con} numeros pares é {s}')
